5693271cc4b92fef027886db4b25fbc08adc724b
Duc Tran <ducth1903@gmail.com>
2020-07-03 16:01:37 -0400

    change index.html container to container-fluid

--- django_family_tree/settings.py
+++ django_family_tree/settings.py
@@ -134,12 +134,12 @@ if os.environ.get("USE_S3")=='True':
     AWS_ACCESS_KEY_ID       = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY   = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
-    # AWS_S3_FILE_OVERWRITE   = True
-    # AWS_DEFAULT_ACL         = None
+    AWS_S3_FILE_OVERWRITE   = False
+    AWS_DEFAULT_ACL         = None
     # AWS_S3_CUSTOM_DOMAIN    = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     # AWS_LOCATION            = ''
     # STATIC_URL              = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}'
-    STATICFILES_STORAGE     = 'storages.backends.s3boto3.S3Boto3Storage'
+    # STATICFILES_STORAGE     = 'storages.backends.s3boto3.S3Boto3Storage'
     DEFAULT_FILE_STORAGE    = 'storages.backends.s3boto3.S3Boto3Storage'
 
     # General optimization for faster delivery
